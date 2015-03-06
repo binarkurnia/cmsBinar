ad9d8a0aabae4cbab51eb58c3aab651892d89948
unknown <binarkurnia97@gmail.com>
2015-03-06 10:33:28 +0700

    perbaikan pertama cmsBinar

---
+++ includes/cmsApplications.php
@@ -0,0 +1,10 @@
+<?php 
+
+require_once('cmsBase');
+//memastikan modul cmsBase hanya di lOAD sekali
+class CmsApplication extends CmsBase {
+	// semua kode yang ada di sini 
+	// dapat diakses melalui fungsi utama
+	// dalam CMS 
+	// management halaman web
+}
